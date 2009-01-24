8226283eb3cf33c2347c9e1f56c8b591af245511
Damien Pobel <damien@pobel.fr>
2009-01-24 18:33:19 +0000

    Added script to cleanup imported elements of the planetarium

---
+++ www/extension/planete/cronjobs/cleanup_planetarium.php
@@ -0,0 +1,33 @@
+<?php
+
+$planetINI = eZINI::instance( 'planete.ini' );
+$planetariumNodeID = $planetINI->variable( 'TreeSettings', 'PlanetariumNodeID' );
+$siteClassID = $planetINI->variable( 'ImportSettings', 'SiteClassID' );
+$postClassID = $planetINI->variable( 'ImportSettings', 'PostClassID' );
+
+$params = array( 'ClassFilterType' => 'include',
+                 'ClassFilterArray' => array( $siteClassID ) );
+
+$planetNodes = eZContentObjectTreeNode::subTreeByNodeID( $params, $planetariumNodeID );
+
+eZLog::setMaxLogSize( $planetINI->variable( 'ImportSettings', 'MaxLogSize' ) );
+$logFile = $planetINI->variable( 'ImportSettings', 'LogFile' );
+
+foreach( $planetNodes as $planet )
+{
+    // keeping only 30 posts from any planet source
+    // to avoid creating the same objet again and again
+    $paramsPost = array( 'ClassFilterType' => 'include',
+                         'ClassFilterArray' => array( $postClassID ),
+                         'SortBy' => array( array( 'published', false ) ),
+                         'Limit' => 10000, // to avoid http://issues.ez.no/14342
+                         'Offset' => 30 );
+    $postNodes = $planet->subTree( $paramsPost );  
+    foreach( $postNodes as $post )
+    {
+        eZLog::write( 'Removing ' . $planet->attribute( 'name' ) . '/' . $post->attribute( 'name' ), $logFile );
+        $post->removeNodeFromTree( false );
+    }
+}
+
+?>
--- www/extension/planete/settings/cronjob.ini.append.php
+++ www/extension/planete/settings/cronjob.ini.append.php
@@ -5,6 +5,7 @@ ExtensionDirectories[]=planete
 
 [CronjobPart-planete]
 Scripts[]=rssimport_planete.php
+Scripts[]=cleanup_planetarium.php
 Scripts[]=staticcache_cleanup.php
 
 */ ?>
