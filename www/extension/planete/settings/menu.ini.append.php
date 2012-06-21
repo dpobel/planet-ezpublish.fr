309845380b9243b91346a94ac9648679a841a3d7
Damien Pobel <dp@ez.no>
2012-06-21 07:58:47 +0200

    Hid media and ezfind tab in the admin interface

---
+++ www/extension/planete/settings/menu.ini.append.php
@@ -0,0 +1,16 @@
+<?php /*
+
+[Topmenu_media]
+Shown[default]=false
+Shown[navigation]=false
+Shown[browse]=false
+Shown[edit]=false
+
+[Topmenu_ezfind]
+Shown[default]=false
+Shown[navigation]=false
+Shown[browse]=false
+Shown[edit]=false
+
+
+ */ ?>
